1e33532d3c5e7fd57d836346d0b17736f6c49647
Martin Helmich <m.helmich@mittwald.de>
2026-02-02 21:51:15 +0100

    chore(cgl): also assert formatting of main plugin file

--- mittwald-ai-provider.php
+++ mittwald-ai-provider.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * Plugin Name: mittwald AI provider
  * Plugin URI: https://github.com/mittwald/wordpress-ai-provider
@@ -13,38 +12,45 @@
 
 // Exit if accessed directly.
 if ( ! defined( 'ABSPATH' ) ) {
-    exit;
+	exit;
 }
 
 /**
  * Display an admin notice if the dependency is missing.
  */
-add_filter('plugin_action_links_'. plugin_basename(__FILE__),  function ( array $links ): array {
-    $settings_link = sprintf(
-            '<a href="%1$s">%2$s</a>',
-            admin_url( 'options-general.php?page=wp-ai-client' ),
-            // ues translation from required plugin `ai`
-            esc_html__( 'Settings', 'ai' )
-    );
+add_filter(
+	'plugin_action_links_' . plugin_basename( __FILE__ ),
+	function ( array $links ): array {
+		$settings_link = sprintf(
+			'<a href="%1$s">%2$s</a>',
+			admin_url( 'options-general.php?page=wp-ai-client' ),
+			// use translation from required plugin `ai`.
+			// phpcs:ignore WordPress.WP.I18n.TextDomainMismatch
+			esc_html__( 'Settings', 'ai' )
+		);
 
-    array_unshift( $links, $settings_link );
+		array_unshift( $links, $settings_link );
 
-    return $links;
-});
+		return $links;
+	}
+);
 
-add_action('wp_loaded', function () {
-    $dep_autoload = WP_PLUGIN_DIR . '/ai/vendor/autoload.php';
-    if ( file_exists( $dep_autoload ) && ! class_exists( '\WordPress\AiClient\AiClient', false ) ) {
-        require_once $dep_autoload;
-    }
+add_action(
+	'wp_loaded',
+	function () {
+		$dep_autoload = WP_PLUGIN_DIR . '/ai/vendor/autoload.php';
+		if ( file_exists( $dep_autoload ) && ! class_exists( '\WordPress\AiClient\AiClient', false ) ) {
+			require_once $dep_autoload;
+		}
 
-    $my_autoload = __DIR__ . '/vendor/autoload.php';
-    if ( file_exists( $my_autoload ) ) {
-        require_once $my_autoload;
-    }
+		$my_autoload = __DIR__ . '/vendor/autoload.php';
+		if ( file_exists( $my_autoload ) ) {
+			require_once $my_autoload;
+		}
 
-    $registry = \WordPress\AiClient\AiClient::defaultRegistry();
-    $registry->registerProvider(\Mittwald\AiProvider\MittwaldAIProvider::class);
+		$registry = \WordPress\AiClient\AiClient::defaultRegistry();
+		$registry->registerProvider( \Mittwald\AiProvider\MittwaldAIProvider::class );
 
-    (new \WordPress\AI_Client\API_Credentials\API_Credentials_Manager())->initialize();
-});
+		( new \WordPress\AI_Client\API_Credentials\API_Credentials_Manager() )->initialize();
+	}
+);
