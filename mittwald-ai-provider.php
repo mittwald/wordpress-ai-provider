80bd77a36680acf40abc35f646e79fee336736bd
Lukas Fritze <l.fritze@mittwald.de>
2026-03-06 13:42:28 +0100

    fix: use different settings page link on WP 7

--- mittwald-ai-provider.php
+++ mittwald-ai-provider.php
@@ -66,9 +66,15 @@ function display_composer_notice(): void {
 add_filter(
 	'plugin_action_links_' . plugin_basename( __FILE__ ),
 	function ( array $links ): array {
+		$settings_page_url = '/wp-admin/options-connectors.php';
+		// TODO: Drop this once we drop WordPress 6.9 support.
+		if ( str_starts_with( wp_get_wp_version(), '6.9' ) ) {
+			$settings_page_url = 'options-general.php?page=wp-ai-client';
+		}
+
 		$settings_link = sprintf(
 			'<a href="%1$s">%2$s</a>',
-			admin_url( 'options-general.php?page=wp-ai-client' ),
+			admin_url( $settings_page_url ),
 			// use translation from required plugin `ai`.
 			// phpcs:ignore WordPress.WP.I18n.TextDomainMismatch
 			esc_html__( 'Settings', 'ai' )
